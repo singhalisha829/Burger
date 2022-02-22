3aec42bde0853aa86d2ab7421a4de8df196ab86b
Alisha Singh <singhalisha829@gmail.com>
2022-02-22 12:02:40 +0530

    Initial commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import classes from './Layout.css';
+import Aux from "../../hoc/Aux";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+
+const layout = (props) => (
+    <Aux>
+        <Toolbar />
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Aux>
+)
+
+export default layout;
