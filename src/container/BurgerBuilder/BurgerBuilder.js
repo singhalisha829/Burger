9a6644757177d7bafc7cea87852c2b6ab15382dd
Alisha Singh <singhalisha829@gmail.com>
2022-03-09 12:15:09 +0530

    button changed

--- src/container/BurgerBuilder/BurgerBuilder.js
+++ src/container/BurgerBuilder/BurgerBuilder.js
@@ -77,7 +77,12 @@ class BurgerBuilder extends Component{
     // }
 
     purchaseHandler =() => {
-        this.setState({purchasing: true})
+        if(this.props.isAuthenticated){
+            this.setState({purchasing: true})
+        }else{
+            this.props.history.push('/auth')
+        }
+        
     }
 
     purchaseCancelHandler =() => {
@@ -141,6 +146,7 @@ class BurgerBuilder extends Component{
                 disabled={disabledInfo}
                 purchaseable={this.updatePurchaseState(this.props.ings)}
                 ordered= {this.purchaseHandler}
+                isAuth={this.props.isAuthenticated}
                 price={this.props.price}/>
                 </Aux>
             );
@@ -169,7 +175,8 @@ const mapStateToProps= state =>{
     return{
         ings:state.burgerBuilder.ingredients,
         price:state.burgerBuilder.totalPrice,
-        error:state.burgerBuilder.error
+        error:state.burgerBuilder.error,
+        isAuthenticated:state.auth.token !== null
     }
 }
 
