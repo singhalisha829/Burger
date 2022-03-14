ff5c108dcd333ce2668e0d8b1704b6007919ad36
Alisha Singh <singhalisha829@gmail.com>
2022-03-14 16:20:50 +0530

    burger img in order added

--- src/components/UI/Order/Order.js
+++ src/components/UI/Order/Order.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
 import classes from './Order.css';
+import Burger from '../../Burger/Burger';
+import BurgerIngredient from '../../Burger/BurgerIngredients/BurgerIngredients';
 
 const order = ( props ) => {
+    //for burger
+    let transformedIngredients= Object.keys(props.ingredients)
+    .map(igKey =>{
+        return [...Array(props.ingredients[igKey])].map((_,i) =>{
+            return <BurgerIngredient key={igKey + i} type={igKey}/>
+        })
+    }).reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>;
+    }
+
+    //for order
     const ingredients = [];
 
     for ( let ingredientName in props.ingredients ) {
@@ -20,7 +36,7 @@ const order = ( props ) => {
                 textTransform: 'capitalize',
                 display: 'inline-block',
                 margin: '0 8px',
-                border: '1px solid #ccc',
+                // border: '1px solid #ccc',
                 padding: '5px'
                 }}
             key={ig.name}>{ig.name} ({ig.amount})</span>;
@@ -28,8 +44,13 @@ const order = ( props ) => {
 
     return (
         <div className={classes.Order}>
-            <p>Ingredients: {ingredientOutput}</p>
-            <p>Price: <strong>USD {Number.parseFloat( props.price ).toFixed( 2 )}</strong></p>
+             <div className={classes.Burger}>
+            <BurgerIngredient type='bread-top' />
+            {transformedIngredients}
+            <BurgerIngredient type='bread-bottom' />
+        </div>
+            <p><strong>Ingredients:</strong> {ingredientOutput}</p>
+            <p><strong>Price: </strong>USD {Number.parseFloat( props.price ).toFixed( 2 )}</p>
         </div>
     );
 };
